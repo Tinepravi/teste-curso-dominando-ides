7cdedce5bbd0734a8479b0fb411fc02121aa1fc8
Tinepravi <tinepravi@live.com>
2021-08-03 20:46:57 -0300

    testando

---
+++ src/com/tinepravi/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package com.tinepravi;
+
+import com.tinepravi.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Gato gato = new Gato();
+	
+	System.out.println(gato);
+	
+		/*int soma = 3 + 2;
+		
+		System.out.println("Alô Mundo, doido!"+" "+ soma);
+	}*/
+
+}
